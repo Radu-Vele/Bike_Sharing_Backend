2d6b205b34b159589fa0ca5627e1167934d6001a
radu_vele <99049451+Radu-Vele@users.noreply.github.com>
2022-10-25 13:03:40 +0300

    Created backend spring project
    Used spring initializr and added Spring Web dependency

---
+++ src/main/java/com/backend/se_project_backend/SeProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.se_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/se_project_backend/SeProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.se_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
