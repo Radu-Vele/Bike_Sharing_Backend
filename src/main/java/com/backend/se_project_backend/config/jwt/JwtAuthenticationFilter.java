4d9c27341f7db79e3cb52f2ad04167c4a4238b3d
radu_vele <99049451+Radu-Vele@users.noreply.github.com>
2023-03-18 17:41:03 +0200

    caught errors in authentication and returned 403

--- src/main/java/com/backend/se_project_backend/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/backend/se_project_backend/config/jwt/JwtAuthenticationFilter.java
@@ -1,8 +1,11 @@
 package com.backend.se_project_backend.config.jwt;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
 import com.mongodb.lang.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -41,38 +44,45 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
      * @throws IOException
      */
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest httpServletRequest, @NonNull HttpServletResponse httpServletResponse, @NonNull FilterChain filterChain) throws ServletException, IOException {
-        String authorization = httpServletRequest.getHeader("Authorization"); //get the JWT
-        String token = null;
-        String username = null;
+    protected void doFilterInternal(@NonNull HttpServletRequest httpServletRequest, @NonNull HttpServletResponse httpServletResponse, @NonNull FilterChain filterChain) throws IOException {
+        try {
+            String authorization = httpServletRequest.getHeader("Authorization"); //get the JWT
+            String token = null;
+            String username = null;
 
-        if(authorization == null || !authorization.startsWith("Bearer")) {
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
-            return; //early return
-        }
-
-        // extract token
-        token = authorization.substring(7); //after keyword Bearer
-        username = jwtUtility.getUsernameFromToken(token);
-
-        if (null != username && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails
-                    = userDetailsService.loadUserByUsername(username);
-
-            if (jwtUtility.isTokenValid(token, userDetails)) {
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-                usernamePasswordAuthenticationToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(httpServletRequest)
-                );
-                //Context holder is updated
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            if(authorization == null || !authorization.startsWith("Bearer")) {
+                filterChain.doFilter(httpServletRequest, httpServletResponse);
+                return; //early return
             }
-        }
 
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+            // extract token
+            token = authorization.substring(7); //after keyword Bearer
+            username = jwtUtility.getUsernameFromToken(token);
+
+
+            if (null != username && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails
+                        = userDetailsService.loadUserByUsername(username);
+
+                if (jwtUtility.isTokenValid(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+                    usernamePasswordAuthenticationToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(httpServletRequest)
+                    );
+                    //Context holder is updated
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+
+        } catch (Exception e) {
+            httpServletResponse.setStatus(HttpStatus.FORBIDDEN.value());
+            httpServletResponse.getWriter().write(e.getMessage());
+        }
     }
 }
